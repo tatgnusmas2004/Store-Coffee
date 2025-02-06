cbbb8e0285a5c23abadf01969c9455b5c149b618
tatgnusmas2004 <fttrat2003@gmail.com>
2025-02-06 22:03:51 +0700

    commit lần đầu

---
+++ CSDL.sql
@@ -0,0 +1,28 @@
+use rat_coffee;
+-- drop table product_toppings;
+-- drop table products;
+-- drop table toppings;
+-- drop table cart_item_toppings;
+-- drop table cart_items;
+-- drop table cart_toppings;
+-- drop table invoice_invo_items;
+-- drop table invoice_item;
+-- drop table invoice;
+ show tables;
+
+-- select * from reservation;
+-- select * from product_toppings;
+-- select * from toppings;	
+-- select * from products;
+-- select * from cart_items;
+
+select * from table_list;
+-- select * from cart_items;
+-- select * from cart_item_toppings;
+-- select * from users;
+select * from invoice;
+select * from invoice_invo_items;
+select * from invoice_item;
+-- describe cart_items;
+
+
